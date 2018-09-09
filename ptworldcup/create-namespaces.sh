9d9c4386a3f631f92dd1ae5a0249230b9c33ff5e
Preston Thompson <preston@appboy.com>
2018-09-09 15:59:29 -0400

    only attempt to create namespace if it doesn't already exist

--- ptworldcup/create-namespaces.sh
+++ ptworldcup/create-namespaces.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
-kubectl create ns teams
-kubectl create ns qotm
-kubectl create ns ambassador
-kubectl create ns grpc-greet
+create-namespace-if-not-exists () {
+  ns=`kubectl get namespace $1 --no-headers --output=go-template={{.metadata.name}} 2>/dev/null`
+  if [ -z "${ns}" ]; then
+    kubectl create ns $1
+  fi
+}
+
+create-namespace-if-not-exists teams
+create-namespace-if-not-exists qotm
+create-namespace-if-not-exists ambassador
+create-namespace-if-not-exists grpc-greet
